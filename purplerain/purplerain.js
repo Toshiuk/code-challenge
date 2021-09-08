893b54a626fcc2ae5f5b6b95bc2929cfc6b29e9c
Flavio Toshiuk Junior <flaviotoshiukhjr@outlook.com>
2021-09-08 22:35:15 +0100

    purplerain plus

--- purplerain/purplerain.js
+++ purplerain/purplerain.js
@@ -7,27 +7,31 @@ let WINDOW_HEIGHT =
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
-const MAX_RAIN_DROPS_NUMBER = 500;
+const MAX_RAIN_DROPS_NUMBER = 1000;
 
 const settings = {
   backgroundColor: "#E6E6FA",
   rainColor: "#8A2BE2",
-  rainDropsNumber: 200,
+  dropEffectColor: "#8A2BE2",
+  rainDropsNumber: 2,
+  dropEffectDistance: 10,
+  maxSpeed: 20,
+  wind: 5,
 };
 
-// let stars;
-// let gui;
+let drops;
+let gui;
 
 function setup() {
   createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
   gui = new dat.GUI();
   gui.add(settings, "rainDropsNumber", 0, MAX_RAIN_DROPS_NUMBER);
-  // gui.add(settings, "velocity", 0, 50);
-  // gui.add(settings, "zoom", 1, 3);
-  // gui.add(settings, "withSteroid");
+  gui.add(settings, "maxSpeed", 1, 40);
+  gui.add(settings, "dropEffectDistance", 0, 20);
+  gui.add(settings, "wind", -20, 20);
   gui.addColor(settings, "backgroundColor");
   gui.addColor(settings, "rainColor");
-  // gui.addColor(settings, "steroidDashColor");
+  gui.addColor(settings, "dropEffectColor");
   drops = new Array(MAX_RAIN_DROPS_NUMBER).fill().map((e) => new Drop());
 }
 
@@ -45,7 +49,6 @@ function windowResized() {
 
 function draw() {
   background(settings.backgroundColor);
-  // translate(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
 
   for (let i = 0; i < settings.rainDropsNumber; i++) {
     drops[i].fall();
@@ -58,23 +61,61 @@ class Drop {
     this.x = random(WINDOW_WIDTH);
     this.y = random(-200, WINDOW_HEIGHT);
     this.z = random(0, 20);
-    this.speed = map(this.z, 0, 20, 1, 15);
+    this.radius = 30;
+    this.maxRadius = random(0, 100);
+    this.dropEffectX = 0;
+    this.speed = map(this.z, 0, 20, 1, settings.maxSpeed);
     this.len = map(this.z, 0, 20, 10, 20);
+  }
+
+  initDropEffect() {
+    this.radius = 2;
+    this.dropEffectX = this.x;
+  }
+
+  dropEffect() {
+    if (this.radius < this.maxRadius) {
+      this.radius += 3;
+      const alpha = map(this.radius, 0, 20, 1, 0.8);
+      stroke(hex2rgba(settings.dropEffectColor, alpha));
+      circle(this.dropEffectX, WINDOW_HEIGHT, this.radius);
+    }
   }
 
   fall() {
     this.y += this.speed;
+    this.x += settings.wind;
     this.speed += map(this.z, 0, 20, 0.01, 0.1);
     if (this.y > WINDOW_HEIGHT) {
+      if (this.z > settings.dropEffectDistance) this.initDropEffect();
       this.y = random(-300, -10);
-      this.speed = map(this.z, 0, 20, 1, 15);
+      this.z = random(0, 20);
+      this.speed = map(this.z, 0, 20, 1, settings.maxSpeed);
+      this.x = random(WINDOW_WIDTH);
+      this.len = map(this.z, 0, 20, 10, 20);
+    }
+
+    if (this.x > WINDOW_WIDTH) {
+      this.x = 0;
+    }
+
+    if (this.x < 0) {
+      this.x = WINDOW_WIDTH;
     }
   }
 
   show() {
-    const thick = map(this.z, 0, 20, 1, 3);
+    const thick = map(this.z, 0, 20, 1, 5);
+    const transparency = map(this.z, 0, 20, 0.1, 0.9);
+    this.dropEffect();
+    noFill();
     strokeWeight(thick);
-    stroke(settings.rainColor);
-    line(this.x, this.y, this.x, this.y + this.len);
+    stroke(hex2rgba(settings.rainColor, transparency));
+    line(this.x, this.y, this.x + settings.wind, this.y + this.len);
   }
 }
+
+const hex2rgba = (hex, alpha = 1) => {
+  const [r, g, b] = hex.match(/\w\w/g).map((x) => parseInt(x, 16));
+  return `rgba(${r},${g},${b},${alpha})`;
+};
